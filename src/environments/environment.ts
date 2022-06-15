6cea14da224bdce31db1ef286b93eeb587cf7f76
Ronak Dumaniya <ronak.dumaniya@gmail.com>
2022-06-15 10:19:06 +0530

    initial

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBVh5LYm5X9ueQ5OlReWJdPZwiQOpkY1oc",
+    authDomain: "labhcloudtest.firebaseapp.com",
+    databaseURL: "https://labhcloudtest.firebaseio.com",
+    projectId: "labhcloudtest",
+    storageBucket: "labhcloudtest.appspot.com",
+    messagingSenderId: "646341923816",
+    appId: "1:646341923816:web:368cdab68aa75b2efaca5d",
+    measurementId: "G-7JS3PBTSCV"
+  }
+};
